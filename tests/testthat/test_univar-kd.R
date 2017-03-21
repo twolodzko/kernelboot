a20950039350a8f0e8abb1ae99b3001ee27659bd
Tymoteusz Wołodźko <twolodzko@gmail.com>
2017-03-21 23:33:14 +0100

    tests

--- tests/testthat/test_univar-kd.R
+++ tests/testthat/test_univar-kd.R
@@ -6,57 +6,60 @@ n <- length(y)
 w <- rep(1/n, n)
 kernel <- "optcosine"
 a <- 1
-lp <- FALSE
 
-lp <- TRUE
-expect_silent(ruvk(10, y, bw, kernel, w, a, lp))
+expect_silent(ruvk(10, y))
 
-lp <- FALSE
+shrink <- TRUE
+expect_silent(ruvk(10, y, bw, kernel, w, a, shrink))
+
+shrink <- FALSE
+expect_silent(ruvk(10, y, bw, kernel, w, a, shrink))
+
 bw <- 1:10
-expect_message(ruvk(10, y, bw, kernel, w, a, lp))
+expect_message(ruvk(10, y, bw, kernel, w, a, shrink))
 
 bw <- -1
-expect_error(ruvk(10, y, bw, kernel, w, a, lp))
+expect_error(ruvk(10, y, bw, kernel, w, a, shrink))
 
 bw <- NA
-expect_error(ruvk(10, y, bw, kernel, w, a, lp))
+expect_error(ruvk(10, y, bw, kernel, w, a, shrink))
 
 bw <- Inf
-expect_error(ruvk(10, y, bw, kernel, w, a, lp))
+expect_error(ruvk(10, y, bw, kernel, w, a, shrink))
 
 bw <- 1
 kernel <- "fluffy cat"
-expect_error(ruvk(10, y, bw, kernel, w, a, lp))
+expect_error(ruvk(10, y, bw, kernel, w, a, shrink))
 
 w <- 1
 kernel <- "gaussian"
-expect_silent(ruvk(10, y, bw, kernel, w, a, lp))
+expect_silent(ruvk(10, y, bw, kernel, w, a, shrink))
 
 w <- rep(-1, n)
-expect_error(ruvk(10, y, bw, kernel, w, a, lp))
+expect_error(ruvk(10, y, bw, kernel, w, a, shrink))
 
 w <- rep(Inf, n)
-expect_error(ruvk(10, y, bw, kernel, w, a, lp))
+expect_error(ruvk(10, y, bw, kernel, w, a, shrink))
 
 w <- rep(NA, n)
-expect_error(ruvk(10, y, bw, kernel, w, a, lp))
+expect_error(ruvk(10, y, bw, kernel, w, a, shrink))
 
 w <- rep(1/n, n-1)
-expect_error(ruvk(10, y, bw, kernel, w, a, lp))
+expect_error(ruvk(10, y, bw, kernel, w, a, shrink))
 
 w <- rep(1/n, n+1)
-expect_error(ruvk(10, y, bw, kernel, w, a, lp))
+expect_error(ruvk(10, y, bw, kernel, w, a, shrink))
 
 w <- rep(1/n, n)
 a <- -1
-expect_error(ruvk(10, y, bw, kernel, w, a, lp))
+expect_error(ruvk(10, y, bw, kernel, w, a, shrink))
 
 w <- rep(1/n, n)
 a <- NA
-expect_error(ruvk(10, y, bw, kernel, w, a, lp))
+expect_error(ruvk(10, y, bw, kernel, w, a, shrink))
 
 w <- rep(1/n, n)
 a <- Inf
-expect_error(ruvk(10, y, bw, kernel, w, a, lp))
+expect_error(ruvk(10, y, bw, kernel, w, a, shrink))
 
 
